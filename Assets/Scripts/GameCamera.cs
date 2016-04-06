05adc6a2c9c27a6c1be44401800f9c450be6ffe2
Josh9309 <joshmalmquist96@gmail.com>
2016-04-05 20:27:27 -0400

    Began Prototyping first Level
    I added the crate and room models into the game. I started setting up
    the first room of the level. The turret script is currently being
    programed it sense whether robit is to the left or right of it but the
    rotation of the turret is a having some problems. I will aim to fix this
    in next commit.

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -5,18 +5,21 @@ public class GameCamera : MonoBehaviour {
 
     public GameObject prey; //this is what the camera will follow
     public float sepDist = 10.0f;
+    public float yDist = 10.0f;
     internal float zPos;
+    internal float yPos;
 	
     // Use this for initialization
 	void Start () {
         zPos = prey.transform.position.z - sepDist;
+        yPos = prey.transform.position.y + yDist;
 	}
 	
 	// Update is called once per frame
 	void Update (){
 
         // Set the position of the camera 
-        Vector3 wantedPosition = new Vector3(prey.transform.position.x, prey.transform.position.y, zPos);
+        Vector3 wantedPosition = new Vector3(prey.transform.position.x, prey.transform.position.y+yDist, zPos);
         transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * 2.0f);
     }
 }
---
+++ Assets/Scripts/TurretScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretScript : MonoBehaviour {
+
+    public GameObject rotater;
+    public GameObject robit;
+    internal float rotSpeed; //rotation speed
+
+	// Use this for initialization
+	void Start () {
+        rotSpeed = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkRobitPos();
+	}
+
+    void checkRobitPos()
+    {
+        if(robit.transform.position.x < this.transform.position.x) //Robit is to the left of the Turret
+        {
+            if(rotater.transform.localRotation.y < 180.0f)
+            {
+                rotater.transform.Rotate(transform.up, -rotSpeed);
+            }
+        }
+        else if (robit.transform.position.x >  this.transform.position.x) //Robit is to the right of the Turret
+        {
+            if(rotater.transform.localRotation.y < 0)
+            {
+                rotater.transform.Rotate(transform.up, rotSpeed);
+            }
+        }
+    }
+}
