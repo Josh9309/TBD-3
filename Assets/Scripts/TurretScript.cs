6d971df4811e514e73501ed7037d0af53239caa2
Josh9309 <joshmalmquist96@gmail.com>
2016-05-25 14:29:39 -0400

    Got Turret Tracking
    Turret in game will now track and follow the player. I also added in a
    skybox to make it look a little cool. A temporary laser has been added
    to turret, this will be improved later. Plan is to start adding in ammo
    and firing next.

--- Assets/Scripts/TurretScript.cs
+++ Assets/Scripts/TurretScript.cs
@@ -5,6 +5,9 @@ public class TurretScript : MonoBehaviour {
 
     public GameObject rotater;
     public GameObject robit;
+    public GameObject gun;
+    public GameObject barrel;
+
     internal float rotSpeed; //rotation speed
 
 	// Use this for initialization
@@ -19,19 +22,64 @@ public class TurretScript : MonoBehaviour {
 
     void checkRobitPos()
     {
-        if(robit.transform.position.x < this.transform.position.x) //Robit is to the left of the Turret
-        {
-            if(rotater.transform.localRotation.y < 180.0f)
-            {
-                rotater.transform.Rotate(transform.up, -rotSpeed);
-            }
-        }
-        else if (robit.transform.position.x >  this.transform.position.x) //Robit is to the right of the Turret
-        {
-            if(rotater.transform.localRotation.y < 0)
-            {
-                rotater.transform.Rotate(transform.up, rotSpeed);
-            }
-        }
+        ////Checks to determine if it should rotate left or right
+        //if(robit.transform.position.x < this.transform.position.x) //Robit is to the left of the Turret
+        //{
+        //    Debug.Log("y: " + rotater.transform.localEulerAngles.y);
+        //    if(rotater.transform.localEulerAngles.y > 180.0f || rotater.transform.localEulerAngles.y == 0)
+        //    {
+        //        rotater.transform.Rotate(transform.up, -rotSpeed);
+        //    }
+        //}
+        //else if (robit.transform.position.x >  this.transform.position.x) //Robit is to the right of the Turret
+        //{
+        //    if(rotater.transform.localEulerAngles.y > 0)
+        //    {
+        //        rotater.transform.Rotate(transform.up, rotSpeed);
+        //    }
+        //    if(rotater.transform.localEulerAngles.y < 0)
+        //    {
+        //        rotater.transform.localRotation = Quaternion.Euler(rotater.transform.localRotation.x, 0, rotater.transform.localEulerAngles.z);
+        //    }
+        //}
+
+        ////Checks to determine if it should rotate up or down
+        //if (robit.transform.position.y < barrel.transform.position.y) //Robbit is bellow gun barrel
+        //{
+        //    Debug.Log("z: " + gun.transform.localEulerAngles.z);
+        //    if(gun.transform.localEulerAngles.z > 342 || gun.transform.localEulerAngles.z <= 17)
+        //    {
+        //        gun.transform.Rotate(transform.forward, -1);
+        //    }
+        //}
+        //else if (robit.transform.position.y > barrel.transform.position.y)//Robit is  above gun barrel
+        //{
+        //    Debug.Log("z: " + gun.transform.localEulerAngles.z);
+        //    if (gun.transform.localEulerAngles.z < 15 || gun.transform.localEulerAngles.z  >=340)
+        //    {
+        //        gun.transform.Rotate(transform.forward, rotSpeed);
+        //    }
+
+        //}
+
+        Vector3 rotaterTar = robit.transform.position - rotater.transform.position;
+        rotaterTar.y = 0;
+
+
+        Quaternion rotaterQuatTar = Quaternion.LookRotation(rotaterTar, Vector3.up);
+        rotater.transform.rotation = Quaternion.RotateTowards(rotater.transform.rotation, rotaterQuatTar, rotSpeed);
+
+        Vector3 gunTar = robit.transform.position; //sets gun target to robits position
+        gunTar.x = 0;// sets the x to 0 so it will not rotate on the x axis
+
+        gun.transform.LookAt(robit.transform, Vector3.up); //rotates the gun to look at robit
+
+        //code that was not working properly Saving to examine it later
+        //Quaternion gunQuatTar = Quaternion.LookRotation(gunTar);
+        //gun.transform.localRotation = Quaternion.RotateTowards(gun.transform.localRotation, gunQuatTar, rotSpeed);
+
+
+
+
     }
 }
